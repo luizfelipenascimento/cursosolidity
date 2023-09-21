b8bde3b8977e804c60a506485e784ec80171703b
Luiz Nascimento <luizfelipesilvadonascimento@gmail.com>
2023-09-21 16:55:09 -0300

    update cadastro to factory new custodia contract for each new client

--- cadastro.sol
+++ cadastro.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+// endereco do contrato rede sepolia: 0x5DD59b5007AB64Db6b071Af0ad90DcE63676a229
+
 contract Cadastro {
     struct Cliente {
         uint256 id;
@@ -18,17 +20,21 @@ contract Cadastro {
     function addClient(
         string memory _nome, 
         string memory _sobrename,
-        address payable _endereco,
         string memory agencia,
         string memory conta 
     ) external returns (bool) {
         string memory agenciaConta = string.concat(agencia, conta);
         bytes memory bytesAgenciaConta = bytes(agenciaConta);
         bytes32 hashTemp = keccak256(bytesAgenciaConta);
-        Cliente memory cliente = Cliente(totalCliente, _nome, _sobrename, _endereco, hashTemp, true);
+        
+        Custodia custodia = new Custodia(hashTemp);
+
+        Cliente memory cliente = Cliente(totalCliente, _nome, _sobrename, payable(address(custodia)), hashTemp, true);
 
         clientes.push(cliente);
         
+        totalCliente++;
+
         return true;
     }
 
@@ -37,4 +43,19 @@ contract Cadastro {
         return (cliente, cliente.existe);
     }
 
+}
+
+contract Custodia {
+
+    bytes32 public hashClient;
+
+    event EtherRecebido();
+
+    constructor(bytes32 _hashClient) {
+        hashClient = _hashClient;
+    }
+
+    receive() external payable {
+        emit EtherRecebido();
+    }
 }
