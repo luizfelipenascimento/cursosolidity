b7e59c02d86d00dfdc29641d6d4bc0200f07a244
Luiz Nascimento <luizfelipesilvadonascimento@gmail.com>
2023-09-22 15:23:34 -0300

    fix

--- contracts/tcc/custodia_cliente.sol
+++ contracts/tcc/custodia_cliente.sol
@@ -2,25 +2,20 @@
 
 pragma solidity 0.8.19;
 
-import "tcc/bradesco_token_aberto.sol";
-
-contract Custodia {
-
-
-
-  bytes32 public hashCliente;// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.19;
-
 import "contracts/tcc/bradesco_token_aberto.sol";
 
+// endereco do contrato na rede sepolia: 0x0882572559A3E2b4f349d4ee387902dfBa458f6c
+
 contract Custodia {
 
-  string public hashCliente;
+  event EtherRecebido();
+
+  bytes32 public hashCliente;
   ExercicioToken exercicioToken;
 
-  constructor(string memory _hashCliente) {
-    hashCliente = _hashCliente;
+  constructor(string memory _nome, string memory _conta, string memory _agencia) {
+    string memory dadosDoCliente = string.concat(_nome, _conta, _agencia);
+    hashCliente = keccak256(bytes(dadosDoCliente));
     exercicioToken = ExercicioToken(0x89A2E711b2246B586E51f579676BE2381441A0d0);
   }
 
@@ -32,26 +27,21 @@ contract Custodia {
     exercicioToken.mint(address(this), quantidade);
   }
   
-  //foi utilizado a contacao de ether para real no dia 21/09/23 18:20
-  function converteTokenToEther() public view returns (uint256) {
-    uint256 saldo = exercicioToken.balanceOf(address(this));
-    uint256 centavos = saldo % 100; 
-    return converteParaEther(saldo / 100) + converteParaEther(centavos) / 100;
+  function transfereEther(uint256 valor, address payable recebedor) public payable {
+    require(valor <= address(this).balance, "saldo insuficiente para operacao");
+    recebedor.transfer(valor);
   }
 
-  function TransfereEther(uint256 quantidade) public payable {
-    converteTokenToEther();
-    require(payable(address(this)).balance >= quantidade, "saldo insuficiente");
-    payable(address(this)).transfer(quantidade);
+  function transfereToken(uint256 valor, address payable recebedor) public payable {
+    require(valor <= exercicioToken.balanceOf(address(this)), "saldo TokenExercicio insuficiente");
+    recebedor.transfer(valor);
   }
 
-  function converteParaEther(uint256 valor) private pure returns (uint256) {
-    return valor * 13 * 10**13;
+  function meuSaldoEther() public view returns (uint256) {
+    return address(this).balance; 
   }
 
-}
-
-  constructor(bytes32 _hashCliente) {
-    hashCliente = _hashCliente;
+  receive() external payable {
+    emit EtherRecebido();
   }
 }
